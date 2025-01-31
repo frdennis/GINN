bd5a114d519ef315161e9f36f5508d6e9d90dbf6
frdennis <dennisfr@euclid21.uio.no>
2025-01-31 14:24:27 +0100

    Uploaded data, made notebook for training

--- model.py
+++ model.py
@@ -1,6 +1,12 @@
 import torch
 import torch.nn as nn
 
+def grad(out, inp):
+    return torch.autograd.grad(out, 
+                               inp, 
+                               grad_outputs=torch.ones_like(out), 
+                               create_graph=True)
+
 
 
 class NNBlock(nn.Module):
@@ -32,32 +38,51 @@ class PINN(nn.Module):
             - returns torch.tensor
     """
     def __init__(self, in_chan, out_chan):
-        super().__init_()
+        super().__init__()
 
+        # Predicts Chi
         self.nn = NNBlock(in_chan, out_chan)
 
         # TODO: Implement trainable model parameters here
-        self.e = torch.nn.Parameter(torch.randn(1))
+        self.e = torch.nn.Parameter(torch.tensor(0.8))
+        self.mu = torch.nn.Parameter(torch.tensor(0.00018))
+        self.M = torch.nn.Parameter(torch.tensor(.04))
 
         # Enable gradient for trainable parameters
         self.e.requires_grad = True
+        self.mu.requires_grad = True
+        self.M.requires_grad = True
 
     def forward(self, x):
-        
-        return self.nn(x)
+        # Output u
+        # TODO: no gradient for model params
+        u = self.mu/self.M * (1 + self.e*torch.cos(self.nn(x)))
+        return u
 
-    def physical_loss(self, y):
+    def physical_loss(self, lambda1=.5, lambda2=.5):
         # TODO: Implement physical loss based on differential equations
-        
-        return y
+        phi = torch.linspace(-16*3.1415, 16*3.1415, steps=1000).view(-1,1).requires_grad_(True)
+        chi = self.nn(phi)
+
+        dchi = grad(chi, phi)[0]
+        ddchi = grad(dchi, phi)[0]
+        # Second derivative
+
+        ode1 = dchi**2 - 1 + 2*self.mu*(3 + self.e*torch.cos(chi))
+        ode2 = ddchi - self.mu*self.e*torch.sin(chi)
+
+        loss1 = torch.mean(ode1**2)
+        loss2 = torch.mean(ode2**2)
+
+        return loss1*lambda1 + loss2*lambda2
 
     
 
 if __name__ == "__main__":
-    nn = NNBlock(1, 1)
+    nn = PINN(1, 1)
     
     x = torch.randn(1, 32, 1)
-    y = nn(x)
-    print(y.shape)
+    print(nn.physical_loss())
+    #print(y.shape)
 
 
