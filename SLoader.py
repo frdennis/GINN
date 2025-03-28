839a69dad45d8c60960ebeb9623925e5e7e2665d
frdennis <dennisfr@euclid21.uio.no>
2025-03-28 15:49:02 +0100

    Added PPINN, Parallell PINN, and fixed code to train in given stars

--- SLoader.py
+++ SLoader.py
@@ -1,25 +1,24 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
 
 
 class SLoader():
-    def __init__(self, files=["data/astrometry_NACO.csv", "data/astrometry_SHARP.csv"], 
+    def __init__(self, star="S2", file_path = 'data/asu.csv',
                  scaling=1e0, augment_data=False, std_data=False):
         # Actions to be done on the data during retrieval
         self.augment_data = augment_data
         self.std_data = std_data
+        self.file_path = file_path
+        self.star = star
         
         # 
         self.distance = 7900 # pc
 
-        self.load_data(files)
+        self.load_data()
         self.projection()
         self.y *= scaling 
         self.x *= scaling
-
 
         self.u = 1/np.sqrt(self.x**2 + self.y**2)
         phi = np.arccos(self.x*self.u) 
@@ -30,7 +29,7 @@ class SLoader():
 
         self.u = torch.tensor(self.u, dtype=torch.float32)
         
-    def load_data(self, files):
+    def load_data(self):
         """
         load_data:
             Loads data and error, and concatenates into tensors for x- and y-coordinates
@@ -38,35 +37,31 @@ class SLoader():
         ARGS:
             * files :   List of files to load. 
         """
-        y = []
-        y_std = []
-        x = []
-        x_std = []
-        for file in files:
-            data = pd.read_csv(file)
+        df = pd.read_csv(self.file_path, sep=',')
+        df.columns = df.columns.str.strip()
 
-            y_ = data.iloc[:,3].values 
-            y_std_ = data.iloc[:,4].values
-            x_ = data.iloc[:,1].values 
-            x_std_ = data.iloc[:,2].values
+        try:
+            x = pd.to_numeric(df['oRA-'+self.star].str.strip(), errors='coerce').values  # RA (arcsec)
+            y = pd.to_numeric(df['oDE-'+self.star].str.strip(), errors='coerce').values  # Dec (arcsec)
+            x_e = pd.to_numeric(df['e_oRA-'+self.star].str.strip(), errors='coerce').values  # RA (arcsec)
+            y_e = pd.to_numeric(df['e_oDE-'+self.star].str.strip(), errors='coerce').values  # Dec (arcsec)
+        except KeyError: 
+            raise KeyError(f"{self.star} is not a registered star in the file {self.file_path}")
 
-            y.append(torch.tensor(y_))
-            y_std.append(torch.tensor(y_std_))
-            x.append(torch.tensor(x_))
-            x_std.append(torch.tensor(x_std_))
+        # remove nans
+        self.x = x[~np.isnan(x)]
+        self.y = y[~np.isnan(y)]        
+        self.x_e = x_e[~np.isnan(x_e)]
+        self.y_e = y_e[~np.isnan(y_e)]
 
-        if len(x) != 1:
-            self.x = torch.cat(x).unsqueeze(1)
-            self.y = torch.cat(y).unsqueeze(1)
-            self.x_std = torch.cat(x_std).unsqueeze(1)
-            self.y_std = torch.cat(y_std).unsqueeze(1)
-        else:
-            self.x = torch.tensor(x[0]).unsqueeze(1)
-            self.y = torch.tensor(y[0]).unsqueeze(1)
-            self.x_std = torch.tensor(x_std[0]).unsqueeze(1)
-            self.y_std = torch.tensor(y_std[0]).unsqueeze(1)
+        # convert to tensor with correct shape
+        self.x = torch.tensor(self.x).unsqueeze(1)
+        self.y = torch.tensor(self.y).unsqueeze(1)
+        self.x_e = torch.tensor(self.x_e).unsqueeze(1)
+        self.y_e = torch.tensor(self.y_e).unsqueeze(1)
 
-    def projection(self, angles=[134.18, 227.85, 66.12]):
+
+    def projection(self):
         """
         project:
 
@@ -75,11 +70,14 @@ class SLoader():
 
         """
         pc_to_au = 206265
-        mas_to_rad = np.pi / (180 * 3600)
+        mas_to_rad = np.pi / (180 * 3600 * 1000)
 
-        i = np.radians(angles[0])
-        Ω = np.radians(angles[1])
-        ω = np.radians(angles[2])
+
+        df = pd.read_csv("data/angles.csv", index_col=0)
+
+        i = np.radians(df.loc[self.star, "i"])
+        Ω = np.radians(df.loc[self.star, "Ω"])
+        ω = np.radians(df.loc[self.star, "ω"])
 
         # Thiele-Innes elements
         A = np.cos(ω)*np.cos(Ω) - np.sin(ω)*np.sin(Ω)*np.cos(i)
@@ -93,8 +91,8 @@ class SLoader():
         X = self.x * mas_to_rad * self.distance * pc_to_au
         Y = self.y * mas_to_rad * self.distance * pc_to_au
 
-        self.x = M_inv[0,0] * (-X) + M_inv[0,1] * Y
-        self.y = M_inv[1,0] * (-X) + M_inv[1,1] * Y
+        self.x = M_inv[0,0] * (X) + M_inv[0,1] * Y
+        self.y = M_inv[1,0] * (X) + M_inv[1,1] * Y
 
 
     def __call__(self):
@@ -102,8 +100,8 @@ class SLoader():
         u = self.u
 
         if self.std_data:
-            x_ = self.x + torch.normal(0, self.x_std)
-            y_ = self.y + torch.normal(0, self.y_std)
+            x_ = self.x + torch.normal(0, self.x_e)
+            y_ = self.y + torch.normal(0, self.y_e)
             u = 1/torch.sqrt(x_**2 + y_**2)
             phi = np.arccos(x_*u) 
             phi = torch.where(y_<0., -phi, phi).float()
@@ -116,14 +114,26 @@ class SLoader():
 
         return torch.tensor(phi, dtype=torch.float32), torch.tensor(u, dtype=torch.float32) # norm const
     
+    def __len__(self):
+        return len(self.phi)
+    
 if __name__ == "__main__":
-    d = SLoader(files=["data/astrometry_NACO.csv", "data/astrometry_SHARP.csv"])
-    #d = SLoader(files=["data/astrometry_NACO.csv"])
+    d = SLoader(star="S2")
 
     x = d.x
     y = d.y    
-    #x, y = d()
+
+    phi = d.phi
+    u = d.u
+
+    print(phi, u)
 
     import matplotlib.pyplot as plt
-    plt.plot(x,y,'kx')
+    fig, ax = plt.subplots(figsize=(10,5), ncols=2)
+    ax[0].plot(phi, u, 'kx')
+    ax[0].grid()
+
+    ax[1].plot(-x,-y,'kx')
+    ax[1].grid()
+    ax[1].axis('equal')
     plt.show()
